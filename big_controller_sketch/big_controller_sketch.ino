47af9b8026e79f6ef59e707948fb7c19e8516622
e-matteson <e.r.matteson@gmail.com>
2025-08-02 14:33:59 -0400

    Add pin numbers to sketch

--- big_controller_sketch/big_controller_sketch.ino
+++ big_controller_sketch/big_controller_sketch.ino
@@ -5,6 +5,32 @@
 
 #include <Wire.h>
 
+int bumper_l = 0; // PA22
+int drv_sleep_n = 1; // PA23
+int io_expander_int_n = 2; // PA10
+int button_extra = 3; // PB10
+int button_a = 4; // PB11
+int button_x = 5; // PA19
+int dpad_left = 6; // PA02
+int button_start = 7; // PB02
+int button_hat_l = 8; // PA18
+int button_back = 9; // PA03
+int button_hat_r = 10; // PA12
+int button_y = 11; // PA13
+int dpad_up = 12; // PA27
+int usr_led_g = 13; // PA09
+int usr_led_r = 14; // PA08
+int dpad_right = 15; // PA28
+int button_home = 16; // PB08
+int dpad_down = 17; // PB09
+int bumper_r = 18; // PA00,
+int button_b = 19; // PA01
+int led_clk = 20; // PA20
+int led_data = 21; // PA21
+
+int ana_trigger_r = 22; // PA11
+int ana_trigger_l = 23; // PB03
+
 TSpi spi{&SPI, 10, {5'000'000, MSBFIRST, SPI_MODE1}}; // max baud rate is 10MHz
 Motor motor {&spi, 3};
 
